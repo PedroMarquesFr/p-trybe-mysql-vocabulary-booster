393e50d15943e0ed47b81bb04a3b6ee2747e8e51
Pedro MF <pedromarques1011@gmail.com>
2021-02-22 18:25:13 -0300

    100%

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,5 @@
-SELECT c.Country "País" FROM w3schools.customers AS c
-WHERE NOT EXISTS (SELECT * FROM w3schools.suppliers AS s WHERE c.Country = s.Country)
-GROUP BY c.Country
-ORDER BY c.Country
+(SELECT Country AS `País` FROM w3schools.suppliers)
+UNION
+(SELECT Country AS `País` FROM w3schools.customers)
+ORDER BY `País`
 LIMIT 5;
